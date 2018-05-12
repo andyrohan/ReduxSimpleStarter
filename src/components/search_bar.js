51ee1677446e226ab82ef38b5fbedd6cd4758925
Andy Rohan <andrewrohan@gmail.com>
2018-05-11 23:53:06 -0400

    Complete the Youtube search app
    Wire up all components and completed functionality

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,26 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { term : ''};
+    }
+
     render() {
-        return <input onChange={event => console.log(event.target.value)} />;
+        return (
+            <div className={'searchbar'}>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.onInputChanged(event.target.value)} />
+            </div>
+        );
+    }
+
+    onInputChanged(term) {
+        this.setState({term});
+        this.props.onSearchTermChanged(term);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,52 @@
+import _ from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 
 const YOUTUBE_API_KEY = 'AIzaSyCjXWSsO3umNGTsfXYzC3eUxlmmOvaFjEE';
 
+
+
 // create a new component that generates HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('frightened rabbit');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: YOUTUBE_API_KEY, term: term}, (videos) => {
+                this.setState({
+                    videos: videos,
+                    selectedVideo: videos[0]
+                });
+            }
+        );
+    }
+
+    render() {
+
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+        return (
+            <div>
+                <SearchBar onSearchTermChanged={videoSearch}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
+            </div>
+        );
+    }
 }
 
 // take the generated HTML and put in on the page, i.e. in the DOM
